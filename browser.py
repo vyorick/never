a9a2d801ca85b9f84f4f8053b0b8dc51da2e0599
Yorick <vyorick@gmail.com>
2015-03-08 15:15:43 +0400

    waits added

--- browser.py
+++ browser.py
@@ -6,6 +6,10 @@ __author__ = 'yorick'
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
 from log import log
 
 logger = log()
@@ -35,11 +39,15 @@ class NW():
 
     def select_character(self, char_name):
         logger.log("page: " + self.driver.title)
-        assert u'Выбор персонажа' in self.driver.title
-        time.sleep(5)
-        elem = self.driver.find_element_by_class_name('charselect')
+        try:
+            elem = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "charselect"))
+            )
+        except:
+            print "element with class 'charselect' not found"
+            return False
+        # elem = self.driver.find_element_by_class_name('charselect')
         # print elem, type(elem), elem.text
-
         links = elem.find_elements_by_tag_name('a')
         assert links
         for link in links:
@@ -74,3 +82,49 @@ class NW():
                 # print link, type(link), link.text
                 link.click()
 
+    def overview_click(self):
+        elem = self.driver.find_element_by_class_name('professions-tab')
+        link = elem.find_element_by_tag_name('a')
+        assert link
+        # print link.text
+        link.click()
+
+    def profession_click(self, profession):
+        '''update-content-professions-1
+        class="professions-tab"
+        '''
+        elem = self.driver.find_element_by_id('update-content-professions-1')
+        # print elem, type(elem), elem.text
+        links = elem.find_elements_by_tag_name('a')
+        assert links
+        for link in links:
+            # print link.text
+            if profession in link.text:
+                # print link, type(link), link.text
+                link.click()
+
+    def collect_rewards(self):
+        '''
+        class="page-professions-overview"
+        class="panel-inner task-slot-progress"
+        class="panel-inner task-slot-finished"
+        button data-url-silent="/professions/collect-reward/1"  Забрать результат
+        :return:
+        '''
+        elem = self.driver.find_element_by_class_name('professions-slots')
+        # slots = tasks_finished = elem.find_elements_by_tag_name('li')
+        # print "slots count:", slots
+        # elem.find_elements_by_partial_link_text('/professions/finish-now')
+        # for slot in xrange(slots):
+        # button = slot.find_elements_by_tag_name('button')
+
+        '''
+        .professions-slots > li:nth-child(1) > span:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(6) > button:nth-child(4)
+        .professions-slots > li:nth-child(2) > span:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(6) > button:nth-child(4)
+        .professions-slots > li:nth-child(5) > span:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(6) > button:nth-child(4)
+        '''
+        tasks_finished = elem.find_elements_by_partial_link_text('collect-reward')
+        print "tasks finished:", len(tasks_finished)
+        tasks_in_progress = elem.find_elements_by_partial_link_text('finish-now')
+        print "tasks in progress:", len(tasks_in_progress)
+        return tasks_finished
